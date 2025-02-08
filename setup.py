a0567f2ad713377459dcedc9d6b67b245a3c4c2a
leegunwoo98 <59849210+leegunwoo98@users.noreply.github.com>
2025-02-08 12:54:33 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "ray",
         'numpy',
         'urlextract',
-        "transformers = "^4.36"
+        "transformers = "^4.36",
         "huggingface-hub",
         'sentence_transformers',
         'torch',
